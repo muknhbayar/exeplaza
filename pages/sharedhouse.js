154e61d02fc027de74a3c8071bfcc0ac7a71df9d
Munkhbayar <mgl0814@gmail.com>
2022-09-27 20:15:16 +0800

    sticky add

--- pages/sharedhouse.js
+++ pages/sharedhouse.js
@@ -78,8 +78,7 @@ active:bg-white active:shadow-lg transition duration-150 ease-in-out" data-bs-to
                           <h5 className="text-xl font-medium leading-normal text-gray-800" id="exampleModalLabel"> Furnishings </h5>
                           <button type="button" className="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
                             data-bs-dismiss="modal" aria-label="Close" /> </div>
-                        <div className="modal-body relative p-4"> Furnishings Modal body text goes here. </div>
-                        
+                        <div className="modal-body relative p-4"> Furnishings Modal body text goes here. </div>                        
                       </div>
                     </div>
                   </div>
