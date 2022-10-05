5bf02d13c1e703552f91b6ae41b3c0f9cbee3023
Munkhbayar <mgl0814@gmail.com>
2022-10-05 18:39:27 +0800

    mongolia top

--- pages/mongolia.js
+++ pages/mongolia.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 import MainLayout from "../layout/MainLayout";
-import LazyLoad from 'react-lazy-load';
+import LazyLoad from "react-lazy-load";
 
 const mongolia = () => {
   return (
@@ -24,7 +24,62 @@ const mongolia = () => {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 mx-auto bg-white max-w-6xl py-16">
-       
+        
+        
+      <div className="flex justify-center col-span-2 mt-16">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+                <div className="flex md:order-last md:ml-16 ">
+                  <img
+                    src="/assets/img/1659508110.jpg"
+                    loading="lazy"
+                    className="transition duration-300 ease-linear align-middle w-full h-72"
+                  />
+                </div>
+                <div className="w-full mr-8 pb-16 pl-8">
+                  <h2 className="font-serif text-3xl text-gray-800 font-bold ">
+                    Education
+                  </h2>
+                  <p className="font-light mt-6 font-sans text-gray-800 max-w-xl ">
+                    After supporting Mongolian educational sector, financing
+                    several school buildings with total capacity of 3,500
+                    students, in 2018 we started operating our own private
+                    school in Mongolia. This is “Olonlog EXE” ITschool, the
+                    first kind of school in Mongolia, in which we teach the IT
+                    curriculum from the elementary school stage. Now in “Olonlog
+                    EXE” school there are nearly 500 bright students, from
+                    elementary school grade 1 to high school, who are striving
+                    to become the innovative power of Mongolia in the digital
+                    era. <br />
+                    Since the establishment within this short period of time the
+                    students showed the great attitude towards the learning
+                    where it is proven by the academic achievement. We have
+                    students who received full scholarships from the US, Canada
+                    and Japanese educational institutions. Not only scholarships
+                    after their high school graduation but during their study in
+                    the school, our students are winning medals constantly from
+                    the various national and provincial level IT, Math and
+                    English language competitions. We believe our students can
+                    become the top international level professionals who will
+                    lead not only in Mongolia but globally.
+                  </p>
+                  <Link href="/education">
+                    <a
+                      className="inline-block px-7 py-4 my-4 bg-sky-500 text-white font-light text-sm leading-snug uppercase rounded hover:bg-sky-400 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+                      role="button"
+                      data-mdb-ripple="true"
+                      data-mdb-ripple-color="light"
+                    >
+                      LEARN MORE
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+
+
+
+
+
         <div className="flex justify-center pt-4 col-span-2">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 lg:mx-16">
             <div>
@@ -36,10 +91,15 @@ const mongolia = () => {
             </div>
             <div className="w-full mx-8 md:mx-16">
               <h2 className="font-serif text-3xl text-gray-800 font-bold ">
-              Finance Industry
+                Finance Industry
               </h2>
               <p className="font-light mt-6 font-sans text-gray-800 max-w-xl ">
-              EXE Corporation is one of the biggest institutional shareholder and board member of BDSec JSC, the biggest brokerage and underwriting firm in Mongolia. Through our local subsidiary Turquoise Finance Non bank financial institution, a financial services company licensed by Mongolian Financial Regulatory Agency, we...
+                EXE Corporation is one of the biggest institutional shareholder
+                and board member of BDSec JSC, the biggest brokerage and
+                underwriting firm in Mongolia. Through our local subsidiary
+                Turquoise Finance Non bank financial institution, a financial
+                services company licensed by Mongolian Financial Regulatory
+                Agency, we...
               </p>
               <Link href="/finance">
                 <a
@@ -53,44 +113,12 @@ const mongolia = () => {
               </Link>
             </div>
 
-            <div className="flex justify-center col-span-2 mt-16">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
-            <div className="flex md:order-last md:ml-16 ">
-              <img
-                src="/assets/img/1659508110.jpg"
-                loading="lazy"
-                className="transition duration-300 ease-linear align-middle w-full h-72"
-              />
-            </div>
-            <div className="w-full mr-8 pb-16 pl-8">
-              <h2 className="font-serif text-3xl text-gray-800 font-bold ">
-                Education
-              </h2>
-              <p className="font-light mt-6 font-sans text-gray-800 max-w-xl ">
-After supporting Mongolian educational sector, financing several school buildings with total capacity of 3,500 students, in 2018 we started operating our own private school in Mongolia. This is “Olonlog EXE” ITschool, the first kind of school in Mongolia, in which we teach the IT curriculum from the elementary school stage. Now in “Olonlog EXE” school there are nearly 500 bright students, from elementary school grade 1 to high school, who are striving to become the innovative power of Mongolia in the digital era. <br/>
-Since the establishment within this short period of time the students showed the great attitude towards the learning where it is proven by the academic achievement. We have students who received full scholarships from the US, Canada and Japanese educational institutions. Not only scholarships after their high school graduation but during their study in the school, our students are winning medals constantly from the various national and provincial level IT, Math and English language competitions. We believe our students can become the top international level professionals who will lead not only in Mongolia but globally.
-</p>
-              <Link href="/education">
-                <a
-                  className="inline-block px-7 py-4 my-4 bg-sky-500 text-white font-light text-sm leading-snug uppercase rounded hover:bg-sky-400 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
-                  role="button"
-                  data-mdb-ripple="true"
-                  data-mdb-ripple-color="light"
-                >
-                  LEARN MORE
-                </a>
-              </Link>
-            </div>
-            </div>
-            </div>
-            
-          
             
           </div>
         </div>
       </div>
     </MainLayout>
-  )
-}
+  );
+};
 
-export default mongolia
+export default mongolia;
