ea98e7bad478be826cf077ecaf388a4b58ddab31
muknhbayar <mgl0814@gmail.com>
2023-01-10 11:18:24 +0800

    mongolia image change

--- pages/mongolia.js
+++ pages/mongolia.js
@@ -9,7 +9,7 @@ const mongolia = () => {
       <div
         className="top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden text-center relative overflow-hidden bg-no-repeat bg-cover"
         style={{
-          backgroundImage: 'url("/assets/img/mv-sm.jpg")',
+          backgroundImage: 'url("/assets/img/mongolia.jpg")',
           backgroundColor: "rgba(0, 0, 0, 0)",
           height: 450,
         }}
